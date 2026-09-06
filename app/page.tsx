0f7a7ef0c2be51bbf612471b0901e453d97c5ccd
evaferreira <evaferreira92@hotmail.com>
2026-09-06 22:14:36 +0000

    break accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
-          alt="Next.js logo"
           width={100}
           height={20}
           priority
